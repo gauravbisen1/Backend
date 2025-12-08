1500260d067bd2f8e7ec888a6d9bd19e750846c2
gauravbisen1 <gouravbisen123@gmail.com>
2025-12-08 22:22:43 +0530

    .

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+import express from "express"
+const app = express()
+
+const connectDB = async () => {
+    try {
+        const connectionString = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionString.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB Connection Failed:", error);
+        process.exit(1); // Exit process with failure 
+    }
+}
+
+export default connectDB;
